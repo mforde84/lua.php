f668afa54bb5b8cd1e3787c5b2745f154ce44570
Martin Forde <mforde84@gmail.com>
2018-04-18 17:05:34 -0500

    Update mysql_query.sh

--- scripts/mysql_query.sh
+++ scripts/mysql_query.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mysql -uroot -p1 <<EOF
+mysql ##################### <<EOF
 DROP DATABASE lua;
 CREATE DATABASE lua;
 USE lua;
@@ -18,7 +18,7 @@ CREATE TABLE software (
 	PRIMARY KEY(id)
 );
 EOF
-for f in $(find /home/blik/Desktop/archive/2018_04_11/apps -type f -name "*lua"); do 
+for f in $(find apps -type f -name "*lua"); do 
  echo $f; 
  export software_name=$(grep "Name:        " $f | sed 's/.*Name:        //g' | sed 's/\")//g');
  export software_version=$(grep Version: $f | sed 's/.* //g' | sed 's/\")//g');
@@ -33,7 +33,7 @@ for f in $(find /home/blik/Desktop/archive/2018_04_11/apps -type f -name "*lua")
  export software_blob=$(sed -n "/Product/,/]])/p" $f | head -n-1 | sed "s/'/_/g" | sed 's/\"/_/g' | sed 's/\`/_/g');
  echo "INSERT INTO software (name, version, compiler, compilerbase, compilerversion, category,description,url, concant, notes) VALUES ('$software_name', '$software_version', '$software_compiler', '$software_compiler_base', '$software_compiler_version', '$software_category', '$software_description', '$software_url', '$software_concant', '$software_blob');" | mysql -uroot -p1 lua;
 done
-mysql -uroot -p1 <<EOF
+mysql ###################3 <<EOF
 USE lua;
 SELECT concant, COUNT(concant) FROM software GROUP BY concant HAVING COUNT(concant) > 1;
 DELETE t1 FROM software t1 INNER JOIN software t2 WHERE t1.id < t2.id AND t1.concant = t2.concant;
